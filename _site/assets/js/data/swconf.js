2ad6ed0e3333ceeefd8cfb331040c6a4fc3a8452
Anes-03 <Anes-03@users.noreply.github.com>
2026-03-14 17:10:30 +0100

    Fix GitHub Pages rendering by removing .nojekyll

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771356246',resources: [
+    cacheName: 'chirpy-1773504626',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
